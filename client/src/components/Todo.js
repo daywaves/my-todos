82371251e0c62b3445a9759ec6ff18b3114a7777
daywaves <daywaves7@gmail.com>
2017-05-21 11:47:46 -0400

    Improve styling

--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -6,7 +6,18 @@ import { TOGGLE_TODO_REQUEST, EDIT_TODO_REQUEST, REMOVE_TODO_REQUEST } from '../
 
 const TodoText = styled.span`
   flex-grow: 1;
-  ${props => (props.completed ? 'text-decoration: line-through; font-style: italic; color: hsla(0, 0%, 0%, 0.7);' : '')}
+  ${props => (props.completed ? 'text-decoration: line-through; font-style: italic; color: hsl(0, 0%, 48%);' : '')}
+  ${props => (props.disabled ? 'color: hsl(0, 0%, 71%);' : '')}
+  transition: color 0.2s;
+`;
+
+const OpacityTransitionButton = styled.button`
+  transition: opacity 250ms;
+`;
+
+// Prevent todo text from moving when checkbox changes to loader
+const CheckboxContainer = styled.div`
+  min-width: 2em;
 `;
 
 class Todo extends Component {
@@ -61,19 +72,21 @@ class Todo extends Component {
         autoFocus
       />
     );
-    const todoLabel = (
-      <TodoText completed={completed} className="is-unselectable">
+    const todoText = (
+      <TodoText completed={completed} className="is-unselectable" disabled={isPending}>
         {text}
       </TodoText>
     );
 
     return (
-      <label htmlFor={inputID} className="panel-block">
-        {togglePending ? <p className="icon"><i className="loader" /></p> : checkbox}
-        {isEditing ? textEditingInput : todoLabel}
+      <label htmlFor={inputID} className="panel-block" disabled={isPending}>
+        <CheckboxContainer>
+          {togglePending ? <p className="icon"><i className="loader" /></p> : checkbox}
+        </CheckboxContainer>
+        {isEditing ? textEditingInput : todoText}
         <div className="field is-grouped">
           <p className="control">
-            <button
+            <OpacityTransitionButton
               className={classnames('button is-small is-warning is-outlined', {
                 'is-hidden': isEditing,
                 'is-loading': editPending,
@@ -84,10 +97,10 @@ class Todo extends Component {
               <span className="icon is-small" aria-label="Edit">
                 <i className="fa fa-pencil" aria-hidden="true" />
               </span>
-            </button>
+            </OpacityTransitionButton>
           </p>
           <p className="control">
-            <button
+            <OpacityTransitionButton
               className={classnames('button is-small is-outlined is-danger', {
                 'is-loading': removePending,
               })}
@@ -97,7 +110,7 @@ class Todo extends Component {
               <span className="icon is-small" aria-label="Delete">
                 <i className="fa fa-times" aria-hidden="true" />
               </span>
-            </button>
+            </OpacityTransitionButton>
           </p>
         </div>
       </label>
