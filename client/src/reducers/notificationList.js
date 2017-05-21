e9535a137c8e501e60de773c07cacead8eee22b0
daywaves <daywaves7@gmail.com>
2017-05-21 15:24:50 -0400

    Limit max number of notifications to 3

--- client/src/reducers/notificationList.js
+++ client/src/reducers/notificationList.js
@@ -1,10 +1,16 @@
 import * as actions from '../actions';
 
+const MAX_LENGTH = 3;
+
 const notificationList = (state = [], action) => {
   switch (action.type) {
     case actions.DISPLAY_NOTIFICATION_BEGIN: {
       const { id, inner, modifierClass } = action;
-      return [...state, { id, inner, modifierClass }];
+      let nextState = state;
+      if (state.length >= MAX_LENGTH) {
+        nextState = nextState.slice(1, MAX_LENGTH);
+      }
+      return [...nextState, { id, inner, modifierClass }];
     }
     case actions.DISPLAY_NOTIFICATION_END:
       return state.filter(notification => notification.id !== action.id);
